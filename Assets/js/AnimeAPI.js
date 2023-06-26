5690ae9084527997eb16d0a80cabdfc5cd073508
Alex Ghezea <alexghezea98@yahoo.com>
2023-06-26 13:33:21 +0300

    Episodes update

--- Assets/js/AnimeAPI.js
+++ Assets/js/AnimeAPI.js
@@ -1,6 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
   const titleElement = document.getElementById("title__selector");
-  const clickedTitle = sessionStorage.getItem("clickedTitle").replace(/[^a-zA-Z0-9]+/g, "-");
+  const clickedTitle = sessionStorage
+    .getItem("clickedTitle")
+    .replace(/[^a-zA-Z0-9]+/g, "-");
 
   if (clickedTitle) {
     titleElement.innerText = clickedTitle;
@@ -8,149 +10,132 @@ window.addEventListener("DOMContentLoaded", () => {
     titleElement.innerText = "No title provided";
   }
 
-
-
-
-    fetch(`https://api.consumet.org/anime/gogoanime/info/${clickedTitle}`)
-    .then(response => response.json())
-    .then(data => {
+  fetch(`https://api.consumet.org/anime/gogoanime/info/${clickedTitle}`)
+    .then((response) => response.json())
+    .then((data) => {
       console.log(data);
       const anime = data;
 
-    if (anime) {
-      const animeTitle = anime.title;
-      titleElement.innerText = animeTitle;
+      if (anime) {
+        const animeTitle = anime.title;
+        titleElement.innerText = animeTitle;
+      } else {
+        titleElement.innerText = "No anime found";
+      }
 
-    } else {
-      titleElement.innerText = "No anime found";
-    }
+      const description = document.querySelector(".description");
+      if (anime.description) {
+        description.innerHTML += anime.description;
+      } else {
+        description.innerHTML = "Description not available";
+      }
 
+      const TVtype = document.querySelector(".type ");
+      if (anime.type) {
+        TVtype.innerHTML += anime.type;
+      } else {
+        TVtype.innerHTML = "Type not available";
+      }
 
-    const description = document.querySelector('.description');
-    if (anime.description) {
-      description.innerHTML += anime.description;
-      
-    } else {
-      description.innerHTML = 'Description not available';
-    }
-    
-    const TVtype = document.querySelector('.type ');
-    if (anime.type) {
-      TVtype.innerHTML += anime.type;
-    } else {
-      TVtype.innerHTML = 'Type not available';
-    }
+      const imgSet = document.querySelector(".anime__details__pic");
+      if (anime.image) {
+        imgSet.style.backgroundImage = `url(${anime.image})`;
+      } else {
+        imgSet.innerHTML = "Image not available";
+      }
 
+      const secondTitle = document.querySelector(".anime__details__title span");
+      if (anime.otherName) {
+        secondTitle.innerHTML += anime.otherName;
+      } else {
+        secondTitle.innerHTML = "Title not available";
+      }
 
-    const imgSet = document.querySelector('.anime__details__pic');
-    if (anime.image) {
-      imgSet.style.backgroundImage = `url(${anime.image})`;
-    } else {
-      imgSet.innerHTML = 'Image not available';
-    }
+      const subOrDub = document.querySelector(".subOrDub");
+      if (anime.subOrDub) {
+        subOrDub.innerHTML += anime.subOrDub;
+      } else {
+        subOrDub.innerHTML = "Studios not available";
+      }
 
+      const airEd = document.querySelector(".aired");
+      if (anime.releaseDate) {
+        airEd.innerHTML += anime.releaseDate;
+      } else {
+        airEd.innerHTML = "Aired date not available";
+      }
 
-    const secondTitle = document.querySelector('.anime__details__title span')
-    if (anime.otherName) {
-      secondTitle.innerHTML += anime.otherName;
-    } else {
-      secondTitle.innerHTML = 'Title not available';
-    }
+      const currentStatus = document.querySelector(".status");
+      if (anime.status) {
+        currentStatus.innerHTML += anime.status;
+      } else {
+        currentStatus.innerHTML = "Status not available";
+      }
 
+      const genre = document.querySelector(".genre");
+      if (anime.genres && anime.genres.length > 0) {
+        genre.innerHTML += anime.genres.slice(0, 3).join(", ");
+      } else {
+        genre.innerHTML = "Genres not available";
+      }
 
-    const subOrDub = document.querySelector('.subOrDub');
-    if (anime.subOrDub) {
-      subOrDub.innerHTML += anime.subOrDub;
-    } else {
-      subOrDub.innerHTML = 'Studios not available';
-    }
-    
-    const airEd = document.querySelector('.aired');
-    if (anime.releaseDate) {
-      airEd.innerHTML += anime.releaseDate;
-    } else {
-      airEd.innerHTML = 'Aired date not available';
-    }
-    
-    const currentStatus = document.querySelector('.status');
-    if (anime.status) {
-      currentStatus.innerHTML += anime.status;
-    } else {
-      currentStatus.innerHTML = 'Status not available';
-    }
-    
-    const genre = document.querySelector('.genre');
-    if (anime.genres && anime.genres.length > 0) {
-      genre.innerHTML += anime.genres.slice(0, 3).join(', ');
-    } else {
-      genre.innerHTML = 'Genres not available';
-    }
-    
-    const episodes = document.querySelector('.episodes');
-    if (anime.totalEpisodes) {
-      episodes.innerHTML += anime.totalEpisodes;
-    } else {
-      episodes.innerHTML = 'Score not available';
-    }
-    
-    const ranks = document.querySelector('.rank');
-    if (anime.rank) {
-      ranks.innerHTML += anime.rank;
-    } else {
-      ranks.innerHTML = 'Rank not available';
-    }
+      const episodes = document.querySelector(".episodes");
+      if (anime.totalEpisodes) {
+        episodes.innerHTML += anime.totalEpisodes;
+      } else {
+        episodes.innerHTML = "Score not available";
+      }
 
+      const ranks = document.querySelector(".rank");
+      if (anime.rank) {
+        ranks.innerHTML += anime.rank;
+      } else {
+        ranks.innerHTML = "Rank not available";
+      }
+    })
+    .catch((error) => console.log(error));
+});
 
+//  $(window).on('load', function () {
+//   $('.filter__controls li').on('click', function () {
+//     $('.filter__controls li').removeClass('active');
+//     $(this).addClass('active');
+//   });
 
-      
-    }
-  )
-  .catch(error => console.log(error));
+//   $('#filterButton').on('click', function () {
+//     const filter = $('.filter__controls li.active').data('filter');
 
+//     if (filter === "*") {
+//       $('.product__item').show();
+//     } else {
+//       $('.product__item').hide();
+//       $(filter).show();
+//     }
+//   });
+// });
+// $(document).ready(function() {
+//   $('.filter__controls li').on('click', function () {
+//     $('.filter__controls li').removeClass('active');
+//     $(this).addClass('active');
+//   });
 
-   });
+//   $('#filterButton').on('click', function () {
+//     const filter = $('.filter__controls li.active').data('filter');
 
+//     if (filter === "*") {
+//       $('.product__item:not(.filtered)').fadeIn();
+//     } else {
+//       $('.product__item').fadeOut(300, function() {
+//         $(this).removeClass('filtered');
+//         $(this).filter(filter).each(function(index) {
+//           $(this).delay(300 * index).fadeIn().addClass('filtered');
+//         });
+//       });
+//     }
+//   });
 
-  //  $(window).on('load', function () {
-  //   $('.filter__controls li').on('click', function () {
-  //     $('.filter__controls li').removeClass('active');
-  //     $(this).addClass('active');
-  //   });
-  
-  //   $('#filterButton').on('click', function () {
-  //     const filter = $('.filter__controls li.active').data('filter');
-      
-  //     if (filter === "*") {
-  //       $('.product__item').show();
-  //     } else {
-  //       $('.product__item').hide();
-  //       $(filter).show();
-  //     }
-  //   });
-  // });
-  // $(document).ready(function() {
-  //   $('.filter__controls li').on('click', function () {
-  //     $('.filter__controls li').removeClass('active');
-  //     $(this).addClass('active');
-  //   });
-  
-  //   $('#filterButton').on('click', function () {
-  //     const filter = $('.filter__controls li.active').data('filter');
-      
-  //     if (filter === "*") {
-  //       $('.product__item:not(.filtered)').fadeIn();
-  //     } else {
-  //       $('.product__item').fadeOut(300, function() {
-  //         $(this).removeClass('filtered');
-  //         $(this).filter(filter).each(function(index) {
-  //           $(this).delay(300 * index).fadeIn().addClass('filtered');
-  //         });
-  //       });
-  //     }
-  //   });
-  
-  //   if ($('.filter__gallery').length > 0) {
-  //     const containerEl = document.querySelector('.filter__gallery');
-  //     const mixer = mixitup(containerEl);
-  //   }
-  // });
+//   if ($('.filter__gallery').length > 0) {
+//     const containerEl = document.querySelector('.filter__gallery');
+//     const mixer = mixitup(containerEl);
+//   }
+// });
--- script.js
+++ script.js
@@ -1,88 +1,85 @@
-
 /*------------------
       OwlCarousel
   --------------------*/
 
 let hero_s = $(".hero__slider");
-    hero_s.owlCarousel({
-        loop: true,
-        margin: 0,
-        items: 1,
-        dots: true,
-        nav: true,
-        navText: ["<span class='fa fa-angle-left'></span>", "<span class='fa fa-angle-right'></span>"],
-        animateOut: 'fadeOut',
-        animateIn: 'fadeIn',
-        smartSpeed: 1200,
-        autoHeight: false,
-        autoplay: true,
-        mouseDrag: false
-    });
+hero_s.owlCarousel({
+  loop: true,
+  margin: 0,
+  items: 1,
+  dots: true,
+  nav: true,
+  navText: [
+    "<span class='fa fa-angle-left'></span>",
+    "<span class='fa fa-angle-right'></span>",
+  ],
+  animateOut: "fadeOut",
+  animateIn: "fadeIn",
+  smartSpeed: 1200,
+  autoHeight: false,
+  autoplay: true,
+  mouseDrag: false,
+});
 
-
- /*------------------
+/*------------------
       MIXIT UP
   --------------------*/
-$(window).on('load', function () {
-    $('.filter__controls li').on('click', function () {
-        $('.filter__controls li').removeClass('active');
-        $(this).addClass('active');
-    });
-    if ($('.filter__gallery').length > 0) {
-        const containerEl = document.querySelector('.filter__gallery');
-        const mixer = mixitup(containerEl);
-    }
+$(window).on("load", function () {
+  $(".filter__controls li").on("click", function () {
+    $(".filter__controls li").removeClass("active");
+    $(this).addClass("active");
+  });
+  if ($(".filter__gallery").length > 0) {
+    const containerEl = document.querySelector(".filter__gallery");
+    const mixer = mixitup(containerEl);
+  }
 });
 
-
- /*------------------
+/*------------------
     SEARCH OVERLAY
   --------------------*/
-$('.search-switch').on('click', function () {
-    $('.search-model').fadeIn(400);
+$(".search-switch").on("click", function () {
+  $(".search-model").fadeIn(400);
 });
 
-$('.search-close-switch').on('click', function () {
-    $('.search-model').fadeOut(400, function () {
-        $('#search-input').val('');
-    });
+$(".search-close-switch").on("click", function () {
+  $(".search-model").fadeOut(400, function () {
+    $("#search-input").val("");
+  });
 });
 
-
-
- /*------------------
+/*------------------
      HAMBURGER NAV
   --------------------*/
 $(document).ready(function () {
-    var trigger = $(".hamburger"),
-      overlay = $(".overlay"),
-      isClosed = false;
-  
-    trigger.click(function () {
-      hamburger_cross();
-    });
-  
-    function hamburger_cross() {
-      if (isClosed == true) {
-        overlay.hide();
-        trigger.removeClass("is-open");
-        trigger.addClass("is-closed");
-        isClosed = false;
-      } else {
-        overlay.show();
-        trigger.removeClass("is-closed");
-        trigger.addClass("is-open");
-        isClosed = true;
-      }
-    }
-  
-    $('[data-toggle="offcanvas"]').click(function () {
-      $("#wrapper").toggleClass("toggled");
-    });
-  });
-  
+  var trigger = $(".hamburger"),
+    overlay = $(".overlay"),
+    isClosed = false;
 
- /*------------------
+  trigger.click(function () {
+    hamburger_cross();
+  });
+
+  function hamburger_cross() {
+    if (isClosed == true) {
+      overlay.hide();
+      trigger.removeClass("is-open");
+      trigger.addClass("is-closed");
+      isClosed = false;
+    } else {
+      overlay.show();
+      trigger.removeClass("is-closed");
+      trigger.addClass("is-open");
+      isClosed = true;
+    }
+  }
+
+  $('[data-toggle="offcanvas"]').click(function () {
+    $("#wrapper").toggleClass("toggled");
+  });
+});
+
+/*------------------
     LOGIN & SWITCHER
   --------------------*/
 const switchers = [...document.querySelectorAll(".switcher")];
@@ -96,45 +93,40 @@ switchers.forEach((item) => {
   });
 });
 
-
 let loginForm = document.getElementById("form-element");
-loginForm.style.display = "none"
+loginForm.style.display = "none";
 
-document.getElementById("login").addEventListener("click", function() {
+document.getElementById("login").addEventListener("click", function () {
   if (loginForm.style.display === "none") {
     loginForm.style.display = "block";
     loginForm.style.posiiton = "fixed";
-    loginForm.style.width = "100%"
-    loginForm.style.height = "100%"
-    loginForm.style.backgroundColor = "rgba(20, 20, 20, 0.8)"
-    loginForm.style.zIndex = "9999"
+    loginForm.style.width = "100%";
+    loginForm.style.height = "100%";
+    loginForm.style.backgroundColor = "rgba(20, 20, 20, 0.8)";
+    loginForm.style.zIndex = "9999";
   } else {
     loginForm.style.display = "none";
   }
 });
 
-$('.login-btn').on('click', function () {
-  $('#login').fadeIn(400);
-  $('#form-element').fadeIn(400);
+$(".login-btn").on("click", function () {
+  $("#login").fadeIn(400);
+  $("#form-element").fadeIn(400);
 });
 
-$('.login-close-btn').on('click', function () {
-  $('#form-element').fadeOut(400);
+$(".login-close-btn").on("click", function () {
+  $("#form-element").fadeOut(400);
 });
 
-  /*------------------
+/*------------------
       Preloader
   --------------------*/
-  $(window).on('load', function () {
-      $(".loader").fadeOut();
-      $("#preloder").delay(200).fadeOut("slow");
-  });
+$(window).on("load", function () {
+  $(".loader").fadeOut();
+  $("#preloder").delay(200).fadeOut("slow");
+});
 
-
-
-
-
- /*------------------
+/*------------------
       ANIME API
   --------------------*/
 // const productTextElements = document.querySelectorAll(".product__item__text, .product__sidebar__rec__item__text, .product__sidebar__view__item");
@@ -168,8 +160,6 @@ $('.login-close-btn').on('click', function () {
 //           console.log(data.data);
 //           const anime = data.data;
 
-
-
 //           const imgElements = document.querySelectorAll(".product__item__pic, .product__sidebar__view__item, .product__sidebar__rec__item__pic");
 //           const imgElement = imgElements[index];
 
@@ -190,17 +180,100 @@ $('.login-close-btn').on('click', function () {
 //     }, index * 1000); // delay of 1 second
 //   }
 // });
+// ............................................................................
+/*------------------
+      ANIME API V.2
+  --------------------*/
+// const productTextElements = document.querySelectorAll(
+//   ".product__item__text, .product__sidebar__rec__item__text, .product__sidebar__view__item"
+// );
 
+// window.addEventListener("DOMContentLoaded", () => {
+//   const titleLinks = document.querySelectorAll("h5 a");
 
+//   titleLinks.forEach((link) => {
+//     link.addEventListener("click", (event) => {
+//       link.href = "/Assets/Pages_of_anime/Details.html";
+//       event.preventDefault();
+//       const clickedTitle = event.target.innerText;
+//       sessionStorage.setItem("clickedTitle", clickedTitle);
+//       // Redirect to the details.html page
+//       window.location.href = event.target.getAttribute("href");
+//     });
+//   });
+// });
 
-const productTextElements = document.querySelectorAll(".product__item__text, .product__sidebar__rec__item__text, .product__sidebar__view__item");
+// productTextElements.forEach((element, index) => {
+//   const titleLink = element.querySelector("h5 a");
+
+//   if (titleLink) {
+//     const title = titleLink.innerText;
+//     const encodedTitle = title.replace(/[^a-zA-Z0-9]+/g, "-");
+
+//     fetch(`https://api.consumet.org/anime/gogoanime/info/${encodedTitle}`)
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log(data);
+//         const anime = data;
+
+//         const genreListElements = element.querySelectorAll("ul li");
+
+//         if (anime.genres && anime.genres.length > 0) {
+//           genreListElements.forEach((liElement, i) => {
+//             if (i < anime.genres.length) {
+//               liElement.textContent = anime.genres[i];
+//             } else {
+//               liElement.style.display = "none";
+//             }
+//           });
+//         } else {
+//           genreListElements.forEach((liElement) => {
+//             liElement.style.display = "none";
+//           });
+//         }
+
+//         const imgElements = document.querySelectorAll(
+//           ".product__item__pic, .product__sidebar__view__item, .product__sidebar__rec__item__pic"
+//         );
+//         const imgElement = imgElements[index];
+
+//         if (anime.image) {
+//           imgElement.style.backgroundImage = `url(${anime.image})`;
+//           if (imgElement.tagName === "DIV") {
+//             const imgTag = imgElement.querySelector("img");
+//             if (imgTag) {
+//               imgTag.src = anime.image;
+//             }
+//           }
+//         } else {
+//           imgElement.innerHTML = "Image not available";
+//         }
+
+//         const epElements = document.querySelectorAll(".product__item__pic .ep");
+//         epElements.forEach((epElement) => {
+//           if (anime.totalEpisodes) {
+//             epElement.textContent = `${anime.totalEpisodes} / ${anime.totalEpisodes}`;
+//           } else {
+//             epElement.style.display = "none";
+//           }
+//         });
+//       })
+
+//       .catch((error) => console.log(error));
+//   }
+// });
+
+const productTextElements = document.querySelectorAll(
+  ".product__item__text, .product__sidebar__rec__item__text, .product__sidebar__view__item"
+);
+const epElements = document.querySelectorAll(".product__item__pic .ep , .ep");
 
 window.addEventListener("DOMContentLoaded", () => {
   const titleLinks = document.querySelectorAll("h5 a");
 
-  titleLinks.forEach(link => {
-    link.addEventListener("click", event => {
-      link.href = "/Assets/Pages_of_anime/Details.html"
+  titleLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      link.href = "/Assets/Pages_of_anime/Details.html";
       event.preventDefault();
       const clickedTitle = event.target.innerText;
       sessionStorage.setItem("clickedTitle", clickedTitle);
@@ -218,13 +291,14 @@ productTextElements.forEach((element, index) => {
     const encodedTitle = title.replace(/[^a-zA-Z0-9]+/g, "-");
 
     fetch(`https://api.consumet.org/anime/gogoanime/info/${encodedTitle}`)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data);
+
         const anime = data;
 
         const genreListElements = element.querySelectorAll("ul li");
-        
+
         if (anime.genres && anime.genres.length > 0) {
           genreListElements.forEach((liElement, i) => {
             if (i < anime.genres.length) {
@@ -239,7 +313,9 @@ productTextElements.forEach((element, index) => {
           });
         }
 
-        const imgElements = document.querySelectorAll(".product__item__pic, .product__sidebar__view__item, .product__sidebar__rec__item__pic");
+        const imgElements = document.querySelectorAll(
+          ".product__item__pic, .product__sidebar__view__item, .product__sidebar__rec__item__pic"
+        );
         const imgElement = imgElements[index];
 
         if (anime.image) {
@@ -253,20 +329,26 @@ productTextElements.forEach((element, index) => {
         } else {
           imgElement.innerHTML = "Image not available";
         }
+
+        if (anime.totalEpisodes) {
+          epElements[
+            index
+          ].textContent = `${anime.totalEpisodes} / ${anime.totalEpisodes}`;
+        } else {
+          epElements[index].style.display = "none";
+        }
       })
-      .catch(error => console.log(error));
+
+      .catch((error) => console.log(error));
   }
 });
 
-
-
-
- /*------------------
+/*------------------
       FOLLOW BUTTON
   --------------------*/
 let follow = document.querySelector(".fa-beat");
 follow.style.display = "none";
-document.querySelector(".fa-beat").addEventListener("click", function() {
+document.querySelector(".fa-beat").addEventListener("click", function () {
   if (follow.style.display === "none") {
     follow.style.display = "block";
   } else {
@@ -274,9 +356,9 @@ document.querySelector(".fa-beat").addEventListener("click", function() {
   }
 });
 
-let secondFollow = document.querySelector(".fa-heart-o")
+let secondFollow = document.querySelector(".fa-heart-o");
 secondFollow.style.display = "inline-block";
-document.querySelector(".fa-heart-o").addEventListener("click", function() {
+document.querySelector(".fa-heart-o").addEventListener("click", function () {
   if (secondFollow.style.display === "inline-block") {
     secondFollow.style.display = "none";
   } else {
@@ -284,36 +366,59 @@ document.querySelector(".fa-heart-o").addEventListener("click", function() {
   }
 });
 
-
-$('.follow-btn').on('click', function () {
-  if ($('.fa-beat').is(':visible')) {
-    $('.fa-beat').fadeOut(1);
-    $('.fa-heart-o').fadeIn(400);
+$(".follow-btn").on("click", function () {
+  if ($(".fa-beat").is(":visible")) {
+    $(".fa-beat").fadeOut(1);
+    $(".fa-heart-o").fadeIn(400);
   } else {
-    $('.fa-beat').fadeIn(400);
-    $('.fa-heart-o').fadeOut(1);
+    $(".fa-beat").fadeIn(400);
+    $(".fa-heart-o").fadeOut(1);
   }
 });
 
+/*------------------
+      COMMENT SECTION
+  --------------------*/
 
+document
+  .getElementById("comment-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-// document.addEventListener('DOMContentLoaded', function() {
-//   var watchNowLink = document.getElementById('watch-now-link');
-//   var videoPlayerDiv = document.querySelector('.anime__video__player');
+    const commentInput = document.getElementById("comment-input").value;
+    if (commentInput.trim() === "") return;
 
-//   watchNowLink.addEventListener('click', function(event) {
-//     event.preventDefault();
+    const newComment = document.createElement("div");
+    newComment.className = "anime__review__item";
 
-//     sessionStorage.clear();
+    const randomProfilePic =
+      "/Assets/Profile/profile" + getRandomNumber(1, 6) + ".jpg";
 
-//     var iframe = document.createElement('iframe');
-//     iframe.src = 'https://www.youtube.com/embed/Ciy0kf5EE3E';
-//     iframe.title = 'Official Trailer | The Eminence in Shadow – 2022 | English Sub';
-//     iframe.frameBorder = '0';
-//     iframe.allow = 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share';
-//     iframe.allowFullscreen = true;
+    const minutesAgo = getMinutesAgo(new Date());
 
-//     videoPlayerDiv.innerHTML = '';
-//     videoPlayerDiv.appendChild(iframe);
-//   });
-// });
+    newComment.innerHTML = `
+    <div class="anime__review__item__pic">
+      <img src="${randomProfilePic}" alt="">
+    </div>
+    <div class="anime__review__item__text">
+      <h6>Your Name - <span>${minutesAgo} minutes ago</span></h6>
+      <p>${commentInput}</p>
+    </div>
+  `;
+
+    const commentSection = document.getElementById("comment-section");
+    if (commentSection.firstChild);
+    commentSection.insertBefore(newComment, commentSection.firstChild);
+
+    document.getElementById("comment-input").value = "";
+  });
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getMinutesAgo(date) {
+  const now = new Date();
+  const diff = (now - date) / 1000; // Difference in seconds
+  return Math.floor(diff / 60); // Convert to minutes
+}
